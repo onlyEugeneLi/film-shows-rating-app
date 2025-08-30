a5b98a4cdb9605c66aca142efd49447a4204bfec
onlyEugeneLi <69563490+onlyEugeneLi@users.noreply.github.com>
2025-08-30 16:34:46 +0100

    Add S3 tf state backend bucket resource

---
+++ module/web-app/locals.tf
@@ -0,0 +1,7 @@
+locals {
+  # S3 backend attributes
+  s3_backend_bucket_name = "app-tf-backend-bucket-55637"
+  s3_backend_state_key = "state/terraform.tfstate"
+  s3_backend_use_lockfile = true
+  s3_backend_encrypt = true
+}
