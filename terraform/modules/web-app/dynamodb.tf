4f87df4529382e40e82eba5da7eb0db855367cd0
onlyEugeneLi <69563490+onlyEugeneLi@users.noreply.github.com>
2025-09-07 19:47:23 +0100

    Initial dynamodb table configuration

---
+++ terraform/modules/web-app/dynamodb.tf
@@ -0,0 +1,42 @@
+# DynamoDB table to store input from web form
+
+resource "aws_dynamodb_table" "dynamodb-table" {
+  name = "film-tracker-app-table"
+  billing_mode = "PROVISIONED"
+  read_capacity = 5
+  write_capacity = 5
+
+  hash_key = "ID"
+
+  # Columns
+  attribute {
+    name = "Title"
+    type = "S"
+  }
+  attribute {
+    name = "Type"
+    type = "S"
+  }
+  attribute {
+    name = "Score"
+    type = "N"
+  }
+  attribute {
+    name = "WatchedDate"
+    type = "S"
+  }
+  attribute {
+    name = "ReleaseYear"
+    type = "S"
+  }
+  attribute {
+    name = "requestTime"
+    type = "S"
+  }
+
+  tags = {
+    name = "film-tracker-app-table"
+  }
+}
+
+# Next: State locking
