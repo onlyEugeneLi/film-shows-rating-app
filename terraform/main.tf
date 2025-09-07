959091168dbb161e2df0d61d9eddcfc7a0364b06
onlyEugeneLi <69563490+onlyEugeneLi@users.noreply.github.com>
2025-09-07 21:57:44 +0100

    Add -auto-approve flag in the command

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 module "web-app" {
   source = "./modules/web-app"
   # Include GitHub Access Token in the command line
-  # terraform apply -var "github_access_token=<paste your token here>"
+  # terraform apply -var "github_access_token=<paste your token here>" -auto-approve
   github_access_token = var.github_access_token
 }
