3f9d452306dd55cd6c735284150b8205e1929077
onlyEugeneLi <69563490+onlyEugeneLi@users.noreply.github.com>
2025-09-07 21:55:30 +0100

    Add appropriate attribute - id

--- terraform/modules/web-app/api_gateway.tf
+++ terraform/modules/web-app/api_gateway.tf
@@ -98,7 +98,7 @@ resource "aws_api_gateway_deployment" "api-gtw-invoke-lambda-deployment" {
 
 # Create a stage
 resource "aws_api_gateway_stage" "api-gtw-invoke-lambda-stage" {
-  deployment_id = aws_api_gateway_deployment.api-gtw-invoke-lambda-deployment
+  deployment_id = aws_api_gateway_deployment.api-gtw-invoke-lambda-deployment.id
   rest_api_id   = aws_api_gateway_rest_api.api-gtw-invoke-lambda.id
   stage_name    = "dev"
 }
