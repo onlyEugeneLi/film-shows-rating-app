40bac9df16107a9fa6765875e2e41b454c2e2d7b
onlyEugeneLi <69563490+onlyEugeneLi@users.noreply.github.com>
2025-09-07 21:56:14 +0100

    Add github_access_token in main.tf to take command line input

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,4 +2,5 @@ module "web-app" {
   source = "./modules/web-app"
   # Include GitHub Access Token in the command line
   # terraform apply -var "github_access_token=<paste your token here>"
+  github_access_token = var.github_access_token
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,8 @@ variable "aws_region" {
   default = "us-east-2"
   type    = string
 }
+
+# dummy variable to take input in the command line
+variable "github_access_token" {
+  
+}
