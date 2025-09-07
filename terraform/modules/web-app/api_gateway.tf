1bb90051b9f6e0e24236294580e8c3a0c693affa
onlyEugeneLi <69563490+onlyEugeneLi@users.noreply.github.com>
2025-09-07 14:04:03 +0100

    Enable CORS

--- terraform/modules/web-app/api_gateway.tf
+++ terraform/modules/web-app/api_gateway.tf
@@ -1,6 +1,6 @@
 # API gateway definition - v1 REST API
 
-# API configuration
+# REST API configuration
 resource "aws_api_gateway_rest_api" "api-gtw-invoke-lambda" {
   name = "api-gtw-invoke-lambda"
   description = "API endpoint used to connnect HTML web and lambda function"
@@ -10,7 +10,7 @@ resource "aws_api_gateway_rest_api" "api-gtw-invoke-lambda" {
   }
 }
 
-# API resource creation
+# API Gateway resource creation
 resource "aws_api_gateway_resource" "api-gtw-invoke-lambda-resource" {
   parent_id = aws_api_gateway_rest_api.api-gtw-invoke-lambda.root_resource_id
   path_part = "/"
@@ -25,6 +25,22 @@ resource "aws_api_gateway_method" "api-gtw-invoke-lambda-method" {
   rest_api_id = aws_api_gateway_rest_api.api-gtw-invoke-lambda.id
 }
 
+# Custom response
+resource "aws_api_gateway_method_response" "api-gtw-invoke-lambda-method-response" {
+  rest_api_id = aws_api_gateway_rest_api.api-gtw-invoke-lambda.id
+  resource_id = aws_api_gateway_resource.api-gtw-invoke-lambda-resource.id
+  http_method = aws_api_gateway_method.api-gtw-invoke-lambda-method.http_method
+  status_code = "200"
+
+  # Enable CORS
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+
+}
+
 # Connect to Lambda
 resource "aws_api_gateway_integration" "api-gtw-invoke-lambda-integration" {
   http_method = aws_api_gateway_method.api-gtw-invoke-lambda-method.http_method
@@ -33,6 +49,30 @@ resource "aws_api_gateway_integration" "api-gtw-invoke-lambda-integration" {
   type = "AWS_PROXY"
   integration_http_method = "POST"
   uri = aws_lambda_function.lambda-function-web-input-dynamodb.invoke_arn
+}
+
+# Integration response
+resource "aws_api_gateway_integration_response" "api-gtw-invoke-lambda-integration-response" {
+  rest_api_id = aws_api_gateway_rest_api.api-gtw-invoke-lambda.id
+  resource_id = aws_api_gateway_resource.api-gtw-invoke-lambda-resource.id
+  http_method = aws_api_gateway_method.api-gtw-invoke-lambda-method.http_method
+  status_code = aws_api_gateway_method_response.api-gtw-invoke-lambda-method-response.status_code
+
+  response_templates = {
+    "application/json" = jsonencode({"LambdaValue"="$input.path('$').body", "data" = "Custom Value"})
+  }
+
+  # Enable CORS
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" =  "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'",
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+
+  depends_on = [
+    aws_api_gateway_integration.api-gtw-invoke-lambda-integration
+  ]
+
 }
 
 # Deploy API
