fc3b01249aa07c99b01c26163bea15d80a8a916c
onlyEugeneLi <69563490+onlyEugeneLi@users.noreply.github.com>
2025-09-10 22:58:07 +0100

    Add aws_region and s3_bucket_name variables

--- terraform/bootstrap-backend/modules/remote-backend/variables.tf
+++ terraform/bootstrap-backend/modules/remote-backend/variables.tf
@@ -1,3 +1,15 @@
+# --------- Global variables: start --------- #
+
+variable "aws_region" {
+    description = "Enter AWS region"
+    type = string
+    default = ""
+}
+
+# --------- Global variables: end --------- #
+
+
+
 # --------- S3 variables: start --------- #
 
 variable "s3_bucket_name" {
@@ -7,8 +19,3 @@ variable "s3_bucket_name" {
 
 # --------- S3 variables: end  --------- #
 
-variable "aws_region" {
-    description = "Enter AWS region"
-    type = string
-    default = "us-east-2"
-}
