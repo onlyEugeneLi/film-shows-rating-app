ea60ae198f17723d36fd9635bbbb0d1e370116a5
onlyEugeneLi <69563490+onlyEugeneLi@users.noreply.github.com>
2025-09-24 23:47:05 +0100

    Update env var format

--- scripts/print_message.sh
+++ scripts/print_message.sh
@@ -3,5 +3,5 @@
 # Run this command to make the script executable for workflow:
 # git update-index --chmod=+x scripts/print_message.sh 
 
-echo "Deploying to ${{ environment }} environment"
+echo "Deploying to $environment environment"
 echo "The workflow is completed."
