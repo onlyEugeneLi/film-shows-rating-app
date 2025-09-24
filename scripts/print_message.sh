ab94553c2abeb4277d96ec97a9548863f75971a8
onlyEugeneLi <69563490+onlyEugeneLi@users.noreply.github.com>
2025-09-24 23:27:39 +0100

    Make script executable and grant GHAS job permission

--- scripts/print_message.sh
+++ scripts/print_message.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Run this command to make the script executable for workflow:
+# git update-index --chmod=+x scripts/print_message.sh 
+
 echo "Deploying to ${{ github.event.inputs.environment }} environment"
 echo "The workflow is completed."
