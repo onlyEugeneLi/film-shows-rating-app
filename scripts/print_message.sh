78ac2487d4290c30e30f7c0617562c4c19816204
onlyEugeneLi <69563490+onlyEugeneLi@users.noreply.github.com>
2025-09-24 23:50:26 +0100

    Correct usage to reference env var at step level - build triggered on push

--- scripts/print_message.sh
+++ scripts/print_message.sh
@@ -3,5 +3,5 @@
 # Run this command to make the script executable for workflow:
 # git update-index --chmod=+x scripts/print_message.sh 
 
-echo "Deploying to $environment environment"
+echo "Deploying to $environment environment" # Environment variable at step level in a bash command is referenced by '$'
 echo "The workflow is completed."
