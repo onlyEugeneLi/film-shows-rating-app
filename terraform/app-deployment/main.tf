e458b97b042e79b01d1367e3e9d49a3e7cdb040f
onlyEugeneLi <69563490+onlyEugeneLi@users.noreply.github.com>
2025-10-06 21:37:26 +0100

    Reformat terraform

--- terraform/app-deployment/main.tf
+++ terraform/app-deployment/main.tf
@@ -14,14 +14,14 @@ terraform {
 
 
   # S3 Backend configuration (Uncomment after S3 bucket provision, run just `terraform init`)
-    backend "s3" {
-      bucket          = "s3-web-app-tf-remote-backend-bucket"
-      key             = "state/terraform.tfstate" # directory path
-      region          = "us-east-2"
-      # dynamodb_table  = "dynamodb-web-app-tf-state-locking-table" # Deprecated - will be made unavailable by Terraform in the future
-      use_lockfile    = true # Recommended by Terraform - Enable state locking via S3
-      encrypt         = true
-    }
+  backend "s3" {
+    bucket = "s3-web-app-tf-remote-backend-bucket"
+    key    = "state/terraform.tfstate" # directory path
+    region = "us-east-2"
+    # dynamodb_table  = "dynamodb-web-app-tf-state-locking-table" # Deprecated - will be made unavailable by Terraform in the future
+    use_lockfile = true # Recommended by Terraform - Enable state locking via S3
+    encrypt      = true
+  }
 
 
   required_providers {
