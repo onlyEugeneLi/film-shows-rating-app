45393791c4d1143986854fa761d789671a438746
onlyEugeneLi <69563490+onlyEugeneLi@users.noreply.github.com>
2025-10-08 23:27:16 +0100

    Improve AWS Amplify initial setup

--- terraform/app-deployment/modules/web-app/amplify.tf
+++ terraform/app-deployment/modules/web-app/amplify.tf
@@ -3,11 +3,9 @@
 # - Relevant S3 Buckets
 
 resource "aws_amplify_app" "amplify_app" {
-  count                         = var.create_amplify_app ? 1 : 0
-  name                          = var.app_name
-  repository                    = var.github_repository_url
-  enable_branch_auto_build      = true
-  auto_branch_creation_patterns = ["dev"]
+  count      = var.create_amplify_app ? 1 : 0
+  name       = var.app_name
+  repository = var.github_repository_url
 
   # used for GitHub repos
   access_token = var.github_access_token // needed for accessing github repo
@@ -16,21 +14,141 @@ resource "aws_amplify_app" "amplify_app" {
   # Redirects for Single Page Web Apps (SPA)
   # https://docs.aws.amazon.com/amplify/latest/userguide/redirects.html#redirects-for-single-page-web-apps-spa
   # The rewrite makes it appear to the user that they have arrived at the original address
+
+  # Enable auto branch creation
+  enable_auto_branch_creation = false
+  enable_branch_auto_build    = true
+  enable_branch_auto_deletion = false
+
   custom_rule {
     source    = "/<*>"
     status    = "404"
     target    = "/index.html"
     condition = null
   }
+
+  tags = {
+    Name        = "Static Website"
+    Environment = "Production"
+  }
 }
 
-# resource "aws_amplify_branch" "amplify_branch_dev" {
-#   count       = var.create_amplify_branch_dev ? 1 : 0
-#   app_id      = aws_amplify_app.amplify_app[0].id
-#   branch_name = var.amplify_branch_dev_name
-#   stage       = var.amplify_branch_dev_stage
+# PROD branch: main
+resource "aws_amplify_branch" "main" {
+  count             = var.create_amplify_branch_prod ? 1 : 0
+  app_id            = aws_amplify_app.amplify_app.id
+  branch_name       = var.amplify_branch_prod_name
+  stage             = var.amplify_branch_prod_stage
+  enable_auto_build = true
+  framework         = var.amplify_app_framework
+  tags = {
+    Branch = var.amplify_branch_prod_name
+  }
+}
+# DEV branch: fix/amplify-index-detection
+resource "aws_amplify_branch" "dev" {
+  count             = var.create_amplify_branch_dev ? 1 : 0
+  app_id            = aws_amplify_app.amplify_app.id
+  branch_name       = var.amplify_branch_dev_name
+  stage             = var.amplify_branch_dev_stage
+  enable_auto_build = true
+  framework         = var.amplify_app_framework
+  tags = {
+    Branch = var.amplify_branch_dev_name
+  }
+}
 
-#   environment_variables = {
-#     ENV = "dev"
-#   }
-# }
+# Enable Web Application Firewall (WAF)
+resource "aws_wafv2_web_acl" "amplify_waf" {
+  name  = "amplify-app-waf"
+  scope = "REGIONAL"
+
+  default_action {
+    allow {}
+  }
+
+  # AWS Managed Rule - Core Rule Set
+  rule {
+    name     = "AWSManagedRulesCommonRuleSet"
+    priority = 1
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesCommonRuleSetMetric"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  # AWS Managed Rule - Known Bad Inputs
+  rule {
+    name     = "AWSManagedRulesKnownBadInputsRuleSet"
+    priority = 2
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesKnownBadInputsRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesKnownBadInputsRuleSetMetric"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  # AWS Managed Rule - Anonymous IP List
+  rule {
+    name     = "AWSManagedRulesAnonymousIpList"
+    priority = 3
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesAnonymousIpList"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesAnonymousIpListMetric"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "amplify-waf-metric"
+    sampled_requests_enabled   = true
+  }
+
+  tags = {
+    Name = "Amplify WAF"
+  }
+}
+
+# Associate WAF with Amplify App
+resource "aws_wafv2_web_acl_association" "amplify_waf_association" {
+  resource_arn = aws_amplify_app.static_website.arn
+  web_acl_arn  = aws_wafv2_web_acl.amplify_waf.arn
+}
--- terraform/app-deployment/modules/web-app/variables.tf
+++ terraform/app-deployment/modules/web-app/variables.tf
@@ -1,4 +1,5 @@
-# - Amplify -
+# ============================================= #
+# --------------  Amplify: START  ------------- #
 variable "create_amplify_app" {
   type        = bool
   default     = true
@@ -9,42 +10,64 @@ variable "app_name" {
   default     = "film-shows-rating-app"
   description = "The name of the Amplify Application"
 }
+variable "amplify_app_framework" {
+  type        = string
+  default     = "Web"
+  description = "Framework for the app"
+}
 
+# Github access
 variable "github_repository_url" {
   type        = string
   default     = "https://github.com/onlyEugeneLi/film-shows-rating-app.git"
   description = "URL for the existing repo"
-
 }
 variable "github_access_token" {
   type = string
-  # default     = null
-  description = "Optional GitHub access token. Only required if using GitHub repo."
 
+  description = "GitHub access token, required when using GitHub repo."
 }
 
+# Dev branch
 variable "create_amplify_branch_dev" {
   type        = bool
   default     = true
   description = "Conditional creation of dev branch for amplify app"
-
 }
 variable "amplify_branch_dev_name" {
   type    = string
-  default = "dev"
+  default = "fix/amplify-index-detection"
 }
 variable "amplify_branch_dev_stage" {
   type    = string
   default = "DEVELOPMENT"
-
 }
 
-# - Lambda - 
+# Prod branch
+variable "create_amplify_branch_prod" {
+  type        = bool
+  default     = false
+  description = "Conditional creation of prod branch for amplify app"
+}
+variable "amplify_branch_prod_name" {
+  type    = string
+  default = "main"
+}
+variable "amplify_branch_prod_stage" {
+  type    = string
+  default = "PRODUCTION"
+}
+# ---------------  Amplify: END  -------------- #
+# ============================================= #
 
+# ============================================= #
+# ---------------  Lambda: START  ------------- #
 variable "lambda_function_name" {
   type        = string
   default     = "write-web-input-to-dynamodb"
   description = "The name of the lambda function that stores data input from web interface to DynamoDB table."
 }
+# ----------------  Lambda: END  -------------- #
+# ============================================= #
 
 # - API Gateway -
