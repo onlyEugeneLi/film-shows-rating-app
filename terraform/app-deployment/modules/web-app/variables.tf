de59d0facb6129c16e82e343b67c44bda487762a
onlyEugeneLi <69563490+onlyEugeneLi@users.noreply.github.com>
2025-10-09 00:27:39 +0100

    Remove WAF

--- terraform/app-deployment/modules/web-app/variables.tf
+++ terraform/app-deployment/modules/web-app/variables.tf
@@ -15,11 +15,6 @@ variable "amplify_app_framework" {
   default     = "Web"
   description = "Framework for the app"
 }
-variable "aws_waf_region" {
-  description = "Amplify WAF can only be set up in us-east-1 region"
-  type = string
-  default = "us-east-1"
-}
 
 # Github access
 variable "github_repository_url" {
