0a2a4700b742916d659e046674643306b987efc8
onlyEugeneLi <69563490+onlyEugeneLi@users.noreply.github.com>
2025-10-08 23:27:32 +0100

    Format the terraform code

--- terraform/app-deployment/terraform.tfvars
+++ terraform/app-deployment/terraform.tfvars
@@ -1,3 +1,3 @@
-aws_region = "us-east-2"
+aws_region                    = "us-east-2"
 s3_remote_backend_bucket_name = "s3-web-app-tf-remote-backend-bucket"
-s3_backend_path = "state/terraform.tfstate"
+s3_backend_path               = "state/terraform.tfstate"
--- terraform/app-deployment/variables.tf
+++ terraform/app-deployment/variables.tf
@@ -10,10 +10,10 @@ variable "github_access_token" {
 # S3 remote backend bucket
 variable "s3_remote_backend_bucket_name" {
   description = "Provide S3 remote backend bucket a unique name"
-  type = string
+  type        = string
 }
 
 variable "s3_backend_path" {
   description = "Set a path for the S3 bucket"
-  type = string
+  type        = string
 }
